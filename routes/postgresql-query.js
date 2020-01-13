cc859a84ec41455d88f11a68d3f5fecf63a3a806
dmc <fukunaga.kenichi@icloud.com>
2020-01-13 15:52:36 +0900

    downgrade pg package

--- routes/postgresql-query.js
+++ routes/postgresql-query.js
@@ -2,49 +2,46 @@ exports.execute = function(req, res) {
 
 	var debug = require('./debug');
 	var pg = require('pg');
+	pg.defaults.ssl = true;
+	var conString = "postgres://"+ req.body.user+":"+ req.body.password + "@" + req.body.host + ":" + req.body.port +"/" + req.body.database;
+
+	var sql;
 	
-	var client = new pg.Pool({
-		host: req.body.host,
-		database: req.body.database,
-		user: req.body.user,
-		password: req.body.password,
-		port:req.body.port
-	})	
-
-	client.connect(function(err) {
-		if(err) {
-			res.statusCode = 503;
-			res.statusMessage = err.message;
-			console.log(err)
-			res.end();
-		}
-	});
-
 	if(req.body.event === "login"){
-		res.statusCode = 200
-		res.end();
+		sql = "SELECT 1";
+	} else {
+		sql = req.body.query;
 	}
-
-	client.query(req.body.query, function(err, result) {
-	
+  
+  	pg.connect(conString, function(err, client, done) {
 		if(err) {
-			debug.log(err);
 			res.statusCode = 503;
 			res.statusMessage = err.message;
 			res.end();
 		}
-		
-		if (result) {
-			console.log(result)
+		client.query(sql, function(err, result) {
+			//call `done()` to release the client back to the pool
+			done();
 
+			if(err) {
+				debug.log(err);
+				res.statusCode = 503;
+				res.statusMessage = err.message;
+				res.end();
+			}
+			
+	
 			if(result.command === "SELECT"){
 				res.json(result.rows);
 				
 			} else {
 				res.json('{"affected":"' + result.rowCount +'"}');
 			}
-		}
 	
-	});
+
+
+		});			
+	});			
+
 	
 }
