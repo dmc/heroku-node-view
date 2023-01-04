62cd09bb95614cd92ab4f3263afc1065c736ad7d
dmc <dmc.system.service@gmail.com>
2023-01-04 14:58:54 +0900

    use jquery from cdn

--- public/javascript/preset.js
+++ public/javascript/preset.js
@@ -1,33 +1,55 @@
 const presets = [
-    [ "", "", "", "", "" ],
-    [ "postgreql-heroku", "postgresql", "ec2-174-129-254-216.compute-1.amazonaws.com",
-            "5432", "da3a0k5tua6qv2", "rzcfpiirtceqhp",
-            "666d25672e8323637b344bbecaf30d6594651788340cf58f9d2fb96e458a4f94" ],
-    [ "mysql-heroku", "mysql", "us-cdbr-iron-east-02.cleardb.net",
-            "3306", "heroku_126abff0eac31c8", "bf05d3bb2265c3",
-            "fc67f770" ],
-    [ "mysql-template", "mysql", "localhost", "3306", "database",
-            "user", "password" ],
-    [ "postgreql-template", "postgresql", "localhost", "5432", "database",
-            "user", "password" ],
-    [ "oracle-template", "oracle", "localhost", "1521", "database",
-            "user", "password" ] ];
+  ["", "", "", "", ""],
+  [
+    "mysql-heroku",
+    "mysql",
+    "us-cdbr-iron-east-02.cleardb.net",
+    "3306",
+    "heroku_126abff0eac31c8",
+    "bf05d3bb2265c3",
+    "fc67f770",
+  ],
+  [
+    "mysql-template",
+    "mysql",
+    "localhost",
+    "3306",
+    "database",
+    "user",
+    "password",
+  ],
+  [
+    "postgreql-template",
+    "postgresql",
+    "localhost",
+    "5432",
+    "database",
+    "user",
+    "password",
+  ],
+  [
+    "oracle-template",
+    "oracle",
+    "localhost",
+    "1521",
+    "database",
+    "user",
+    "password",
+  ],
+];
 
-const csspresets =
-    ["default.css",
-    "dark.css",
-    "light.css",
-    "terminal.css"];
+const csspresets = ["default.css", "mono.css", "terminal.css"];
 
 function loadPreset() {
-    for (var i = 0; i < presets.length; i++) {
-        $("#preset").append($('<option>').html(presets[i][0]).val(i));
-    }
+  for (var i = 0; i < presets.length; i++) {
+    $("#preset").append($("<option>").html(presets[i][0]).val(i));
+  }
 
-    for (var i = 0; i < csspresets.length; i++) {
-        var $option = $('<option>').html(csspresets[i]).val(csspresets[i])
-                .attr('id', csspresets[i]);
-        $("#css-list").append(
-                $('<option>').html(csspresets[i]).val(csspresets[i]));
-    }
+  for (var i = 0; i < csspresets.length; i++) {
+    $("<option>")
+      .html(csspresets[i])
+      .val(csspresets[i])
+      .attr("id", csspresets[i]);
+    $("#css-list").append($("<option>").html(csspresets[i]).val(csspresets[i]));
+  }
 }
--- routes/mysql-query.js
+++ routes/mysql-query.js
@@ -1,34 +1,26 @@
 exports.execute = function (req, res) {
+  const mysql = require("mysql");
 
-    const mysql = require('mysql');
+  const sql = req.body.event === "login" ? "SELECT 1" : req.body.query;
+  const connection = mysql.createConnection({
+    host: req.body.host,
+    port: req.body.port,
+    user: req.body.user,
+    password: req.body.password,
+    database: req.body.database,
+  });
 
-    const connection = mysql.createConnection({
-        host: req.body.host,
-        port: req.body.port,
-        user: req.body.user,
-        password: req.body.password,
-        database: req.body.database
-    });
+  connection.connect();
+  connection.query({ sql: sql }, (err, results) => {
+    if (err) {
+      res.statusCode = 503;
+      res.statusMessage = err.message;
+      res.end();
+      connection.end();
+      return;
+    }
 
-    const sql = req.body.event === "login" ? "SELECT 1" : req.body.query;
-
-    connection.connect();
-    connection.query({
-        sql: sql
-    }, function (err, results) {
-        if (err) {
-            res.statusCode = 503;
-            res.statusMessage = err.message;
-            res.end();
-        }
-
-        if (results.affectedRows) {
-            res.json('{"affected":"' + results.affectedRows + '"}');
-        } else {
-            res.json(results);
-        }
-    });
-
+    res.json(results);
     connection.end();
-
+  });
 };
